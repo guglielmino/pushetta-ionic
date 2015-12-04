8086fccd06c21da9341c7a048ef2bfe003ae835f
Fabrizio Guglielmino <guglielmino@gmail.com>
2015-12-04 10:38:28 +0100

    Sections in subscriptions view

--- www/js/directives/dividerSubscriptions.js
+++ www/js/directives/dividerSubscriptions.js
@@ -6,9 +6,8 @@ angular.module('pushetta')
         };
 
         function compile(element, attr) {
-            attr.class = "";
             element.children().attr('ng-hide', 'subscription.isDivider');
-            //element.children().remove();
+
             element.append(
                 '<div class="item-divider ng-hide" ng-show="subscription.isDivider" ng-bind="subscription.divider"></div>'
             );
